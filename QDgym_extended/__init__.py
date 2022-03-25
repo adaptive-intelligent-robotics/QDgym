38fbe81ae8a2d5d96d7667dd1af13daf32921e13
Manon Flageat <manon.flageat18@imperial.ac.uk>
2022-03-25 00:50:19 +0000

    refactor!: change framework name for QDgym_extended

--- QDgym_extended/__init__.py
+++ QDgym_extended/__init__.py
@@ -2,8 +2,6 @@ import logging
 import gym
 from gym.envs.registration import registry, make, spec
 
-
-
 def register(id, *args, **kvargs):
   if id in registry.env_specs:
     return
@@ -14,34 +12,32 @@ def register(id, *args, **kvargs):
 # ------------QDgym-------------
 
 register(id='QDWalker2DBulletEnv-v0',
-         entry_point='QDgym.QDgym_envs:QDWalker2DBulletEnv',
+         entry_point='QDgym_extended.QDgym_envs:QDWalker2DBulletEnv',
          max_episode_steps=1000,
          reward_threshold=2500.0)
 
 
 register(id='QDHalfCheetahBulletEnv-v0',
-         entry_point='QDgym.QDgym_envs:QDHalfCheetahBulletEnv',
+         entry_point='QDgym_extended.QDgym_envs:QDHalfCheetahBulletEnv',
          max_episode_steps=1000,
          reward_threshold=3000.0)
 
 
 register(id='QDAntBulletEnv-v0',
-         entry_point='QDgym.QDgym_envs:QDAntBulletEnv',
+         entry_point='QDgym_extended.QDgym_envs:QDAntBulletEnv',
          max_episode_steps=1000,
          reward_threshold=2500.0)
 
 
 register(id='QDHopperBulletEnv-v0',
-         entry_point='QDgym.QDgym_envs:QDHopperBulletEnv',
+         entry_point='QDgym_extended.QDgym_envs:QDHopperBulletEnv',
          max_episode_steps=1000,
          reward_threshold=2500.0)
 
 
 register(id='QDHumanoidBulletEnv-v0',
-         entry_point='QDgym.QDgym_envs:QDHumanoidBulletEnv',
+         entry_point='QDgym_extended.QDgym_envs:QDHumanoidBulletEnv',
          max_episode_steps=1000)
-
-
 
 
 def getList():
--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
 setup(
-    name="QDgym",
-    url="https://github.com/ollenilsson19/QDgym",
+    name="QDgym_extended",
+    url="https://github.com/adaptive-intelligent-robotics/QDgym_extended.git",
     author="Olle Nilsson",
     author_email="olle.nilsson19@imperial.ac.uk",
-    packages=["QDgym"],
+    packages=["QDgym_extended"],
     install_requires=["gym==0.15.4", "pybullet==3.0.8", "numpy==1.19.5"],
-    package_data={"QDgym":["assets/*"]},
+    package_data={"QDgym_extended":["assets/*"]},
     version="0.1",
     license="MIT",
-    description="QDgym",   
+    description="QDgym_extended",   
 )
