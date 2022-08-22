d48e18ac564dce5efd757573eab12cdb9a013231
Manon Flageat <manon.flageat18@imperial.ac.uk>
2022-08-22 12:40:18 +0000

    Update setup.py requirements to >=

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(
     name="QDgym_extended",
     url="https://github.com/adaptive-intelligent-robotics/QDgym_extended.git",
-    author="Olle Nilsson",
-    author_email="olle.nilsson19@imperial.ac.uk",
+    author="Olle Nilsson, Bryan Lim, Manon Flageat",
+    author_email="olle.nilsson19@imperial.ac.uk, bryan.lim16@imperial.ac.uk, manon.flageat18@imperial.ac.uk",
     packages=["QDgym_extended"],
-    install_requires=["gym==0.15.4", "pybullet==3.0.8", "numpy==1.19.5"],
+    install_requires=["gym>=0.15.4", "pybullet>=3.0.8", "numpy>=1.19.5"],
     package_data={"QDgym_extended":["assets/*"]},
     version="0.1",
     license="MIT",
